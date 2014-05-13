7587f8ce1da4267000eb033d39f8a31f35158d8a
byelipk <byelipk@gmail.com>
2014-05-13 19:37:19 -0400

    create filter for active todos

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -3,6 +3,6 @@
 App.Router.map(function() {
   // Make `/` go to `todos` resource
   this.resource('todos', { path: '/' }, function() {
-
+    this.route('active')
   });
 });
---
+++ app/assets/javascripts/routes/todos/active_route.js
@@ -0,0 +1,18 @@
+// For more information see: http://emberjs.com/guides/routing/
+
+App.TodosActiveRoute = Ember.Route.extend({
+  model: function() {
+    // For info on DS.Store.filter see:
+    // http://emberjs.com/api/data/classes/DS.Store.html#method_filter
+    return this.store.filter('todo', function(todo) {
+      // We want the callback function to return `false`
+      return !todo.get('isCompleted');
+    });
+  },
+
+  renderTemplate: function(controller) {
+    // Render todos.index and supply it the current ArrayController so it
+    // uses the collection of uncompleted todos.
+    return this.render('todos/index', { controller: controller });
+  }
+});
