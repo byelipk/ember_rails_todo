a90098d307713b58102f1bf62f36c683ebbac67b
byelipk <byelipk@gmail.com>
2014-05-12 22:24:16 -0400

    rename namespace on todo.js

--- app/assets/javascripts/models/todo.js
+++ app/assets/javascripts/models/todo.js
@@ -1,6 +1,6 @@
 // for more details see: http://emberjs.com/guides/models/defining-models/
 
-TodoMVC.Todo = DS.Model.extend({
+App.Todo = DS.Model.extend({
   title: DS.attr('string'),
   isCompleted: DS.attr('boolean')
 });
