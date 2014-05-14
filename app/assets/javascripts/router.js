c62a5bb30e3ba09a27a62f42d32d0571fb40c9d4
byelipk <byelipk@gmail.com>
2014-05-13 20:19:35 -0400

    link up todos.index

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -5,5 +5,7 @@ App.Router.map(function() {
   this.resource('todos', { path: '/' }, function() {
     this.route('active');
     this.route('completed');
+
+    // We get `todos.index` for free when use the callback
   });
 });
