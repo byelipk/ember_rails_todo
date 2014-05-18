a63dfdbda01d7289eaa06976ad3f4a1cd8820979
byelipk <byelipk@gmail.com>
2014-05-18 13:25:24 -0400

    more todo unit tests, one todo controller test

--- test/models/todo_test.rb
+++ test/models/todo_test.rb
@@ -6,8 +6,31 @@ class TodoTest < ActiveSupport::TestCase
     assert_equal nil, todo.title
   end
 
-  test "should not save without title" do
+  test "new todo should not save without title" do
     todo = Todo.new
     assert_not todo.save, "Saved todo without a title"
+  end
+
+  test "new todo should not be completed" do
+    todo = Todo.new
+    assert_equal false, todo.is_completed?
+  end
+
+  test "can set a new title" do
+    todo = Todo.new
+    todo.title = "Example"
+    assert_equal "Example", todo.title
+  end
+
+  test "can toggle #is_completed" do
+    todo = Todo.new
+    todo.is_completed = true
+    assert_equal true, todo.is_completed?
+  end
+
+  test "a todo with a title is valid" do
+    todo = Todo.new
+    todo.title = "Example"
+    assert_equal true, todo.save
   end
 end
