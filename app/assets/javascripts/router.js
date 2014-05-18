887450d4ae178473f2fa5917c419e7c2193ebec5
byelipk <byelipk@gmail.com>
2014-05-18 01:31:45 -0400

    add comment to router.js

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -3,6 +3,9 @@
 App.Router.map(function() {
   // Make `/` go to `todos` resource
   this.resource('todos', { path: '/' }, function() {
+
+    // In general routes, as opposed to resources, might be good candidates
+    // for store.filter or store.filterBy.
     this.route('active');
     this.route('completed');
 
